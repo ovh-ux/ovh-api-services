f9368f00fc287b87daff0e093506e59c18655a94
Jean-Frederic Durand <jean-frederic.durand@corp.ovh.com>
2017-08-01 11:57:15 -0400

    feature(cloudMigration) : implements cloud migration

---
+++ src/cloud/project/migration/cloud-project-migration.lexi.service.js
@@ -0,0 +1,34 @@
+angular.module("ovh-api-services").service("CloudProjectMigrationLexi", function ($resource, $cacheFactory) {
+    "use strict";
+
+    var queryCache = $cacheFactory('CloudProjectMigrationLexiQuery'),
+        cache = $cacheFactory('CloudProjectMigrationLexi');
+
+    var interceptor = {
+        response: function (response) {
+            cache.remove(response.config.url);
+            queryCache.removeAll();
+            return response.data;
+        }
+    };
+
+    var migration = $resource("/cloud/project/:serviceName/migration/:migrationId", {
+        serviceName : '@serviceName',
+        migrationId : '@migrationId'
+    }, {
+        get   : { method: 'GET', cache: cache },
+        query : { method: 'GET', cache: queryCache, isArray: true },
+        put   : { method: "PUT", interceptor : interceptor }
+    });
+
+    migration.resetCache = function() {
+        cache.removeAll();
+    };
+
+    migration.resetQueryCache = function() {
+        queryCache.removeAll();
+    };
+
+    return migration;
+
+});
