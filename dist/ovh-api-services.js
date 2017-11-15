f1d73ccaa5a66c2f5f53204391f0fd98ec1c2670
CDS Agent <cds@ovh.net>
2017-11-15 15:07:09 +0000

    Release v2.1.8

--- dist/ovh-api-services.js
+++ dist/ovh-api-services.js
@@ -1215,6 +1215,50 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectIpFailover", ["$in
 
 }]);
 
+angular.module("ovh-api-services").service("OvhApiCloudProjectIplbLexi", ["$resource", "$cacheFactory", function ($resource, $cacheFactory) {
+    "use strict";
+
+    var queryCache = $cacheFactory("OvhApiCloudProjectIplbLexiQuery");
+    var cache = $cacheFactory("OvhApiCloudProjectIplbLexi");
+
+    var loadbalancers = $resource("/cloud/project/:serviceName/ipLoadbalancing/:id", {
+        serviceName: "@serviceName",
+        id: "@id"
+    }, {
+        get: { method: "GET", cache: cache },
+        post: { method: "POST" },
+        validate: { method: "POST", url: "/cloud/project/:serviceName/ipLoadbalancing/:id/validate" },
+        query: {
+            method: "GET",
+            cache: queryCache,
+            isArray: true
+        }
+    });
+
+    loadbalancers.resetCache = function () {
+        cache.removeAll();
+    };
+
+    loadbalancers.resetQueryCache = function () {
+        queryCache.removeAll();
+    };
+
+    return loadbalancers;
+
+}]);
+
+angular.module("ovh-api-services").service("OvhApiCloudProjectIplb", ["$injector", function ($injector) {
+
+    "use strict";
+
+    return {
+        Lexi: function () {
+            return $injector.get("OvhApiCloudProjectIplbLexi");
+        }
+    };
+
+}]);
+
 angular.module("ovh-api-services").service("OvhApiCloudProjectMigrationLexi", ["$resource", "$cacheFactory", function ($resource, $cacheFactory) {
     "use strict";
 
