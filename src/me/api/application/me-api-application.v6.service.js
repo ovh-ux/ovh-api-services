7091b820a3b2b56e6575c9d3704928269d877706
Antoine Leblanc <ant.leblanc@gmail.com>
2018-04-20 16:55:04 +0200

    feat(me api): add application and credential api bindings (#75)
    ref: ovh-ux/ovh-manager-dedicated#191

---
+++ src/me/api/application/me-api-application.v6.service.js
@@ -0,0 +1,61 @@
+angular.module("ovh-api-services").service("OvhApiMeApiApplicationV6", function ($cacheFactory, $resource) {
+    "use strict";
+
+    var queryCache = $cacheFactory("OvhApiMeApiApplicationV6Query");
+    var cache = $cacheFactory("OvhApiMeApiApplicationV6");
+    var batchCache = $cacheFactory("OvhApiMeApiApplicationV6Batch");
+
+    var interceptor = {
+        response: function (response) {
+            cache.remove(response.config.url);
+            queryCache.removeAll();
+            return response.data;
+        }
+    };
+
+    var resource =  $resource("/me/api/application/:applicationId", {
+        applicationId: "@applicationId"
+    }, {
+        query: {
+            method: "GET",
+            isArray: true,
+            cache: queryCache
+        },
+        get: {
+            method: "GET",
+            cache: cache
+        },
+        getBatch: {
+            method: "GET",
+            isArray: true,
+            cache: batchCache,
+            headers: {
+                "X-Ovh-Batch": ","
+            }
+        },
+        "delete": {
+            method: "POST",
+            interceptor: interceptor
+        }
+    });
+
+    resource.resetQueryCache = function () {
+        queryCache.removeAll();
+    };
+
+    resource.resetCache = function () {
+        cache.removeAll();
+    };
+
+    resource.resetBatchCache = function () {
+        batchCache.removeAll();
+    };
+
+    resource.resetAllCache = function () {
+        this.resetQueryCache();
+        this.resetCache();
+        this.resetBatchCache();
+    };
+
+    return resource;
+});
