236a89b50a61a27db1d6f798215f52729d270fd2
CDS Agent <ant.leblanc@gmail.com>
2018-08-09 20:00:28 +0000

    Release v3.12.2

--- dist/ovh-api-services.js
+++ dist/ovh-api-services.js
@@ -5484,6 +5484,29 @@ angular.module("ovh-api-services").service("OvhApiDedicatedCloudV6", ["$resource
             url: "/dedicatedCloud/:serviceName/hcx",
             method: "GET",
             cache: cache
+        },
+        getGlobalTasks: {
+            url: "/dedicatedCloud/:serviceName/globalTasks",
+            method: "GET",
+            isArray: true,
+            queryParams: {
+                datacenterId: "@datacenterId",
+                "endDate.from": "@endDate.from",
+                "endDate.to": "@endDate.to",
+                "executionDate.from": "@executionDate.from",
+                "executionDate.to": "@executionDate.to",
+                filerId: "@filerId",
+                hostId: "@hostId",
+                "lastModificationDate.from": "@lastModificationDate.from",
+                "lastModificationDate.to": "@lastModificationDate.to",
+                name: "@name",
+                networkAccessId: "@networkAccessId",
+                orderId: "@orderId",
+                parentTaskId: "@parentTaskId",
+                state: "@state",
+                userId: "@userId",
+                vlanId: "@vlanId"
+            }
         }
     });
 
