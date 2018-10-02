ce20fbeeea59ca172f488a6cd6809d2c8758e57d
CDS Agent <ant.leblanc@gmail.com>
2018-10-02 15:23:23 +0000

    Release v3.16.1

--- dist/ovh-api-services.js
+++ dist/ovh-api-services.js
@@ -6300,13 +6300,41 @@ angular.module("ovh-api-services").service("OvhApiDedicatedCloudVMEncryptionKmsV
 
     var queryCache = $cacheFactory("OvhApiDedicatedCloudVMEncryptionKmsV6Query");
     var cache = $cacheFactory("OvhApiDedicatedCloudVMEncryptionKmsV6");
+    var interceptor = function (response) {
+        cache.remove(response.config.url);
+        queryCache.removeAll();
+        return response;
+    };
 
     var kmsResource = $resource("/dedicatedCloud/:serviceName/vmEncryption/kms/:kmsId", {
         serviceName: "@serviceName",
         kmsId: "@kmsId"
     }, {
         query: { method: "GET", cache: queryCache, isArray: true },
-        get: { method: "GET", cache: cache }
+        get: { method: "GET", cache: cache },
+        create: {
+            method: "POST",
+            url: "/dedicatedCloud/:serviceName/vmEncryption/kms",
+            params: {
+                ip: "@ip",
+                description: "@description",
+                sslThumbprint: "@sslThumbprint"
+            },
+            interceptor: interceptor
+        },
+        changeProperties: {
+            method: "POST",
+            url: "/dedicatedCloud/:serviceName/vmEncryption/kms/:kmsId/changeProperties",
+            params: {
+                description: "@description",
+                sslThumbprint: "@sslThumbprint"
+            },
+            interceptor: interceptor
+        },
+        "delete": {
+            method: "DELETE",
+            interceptor: interceptor
+        }
     });
 
     kmsResource.resetQueryCache = function () {
