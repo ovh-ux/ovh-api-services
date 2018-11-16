69f767d598bc53c81ba6934fa139fe82db7f6fbb
Marie JONES <marie.jones@corp.ovh.com>
2018-11-16 16:39:47 +0100

    feat(email.exchange): add apiv6 basic routes

--- src/email/exchange/service/email-exchange-service.service.js
+++ src/email/exchange/service/email-exchange-service.service.js
@@ -1,6 +1,9 @@
 angular.module("ovh-api-services").service("OvhApiEmailExchangeService", function ($injector) {
     "use strict";
     return {
+        v6: function () {
+            return $injector.get("OvhApiEmailExchangeServiceV6");
+        },
         v7: function () {
             return $injector.get("OvhApiEmailExchangeServiceV7");
         }
