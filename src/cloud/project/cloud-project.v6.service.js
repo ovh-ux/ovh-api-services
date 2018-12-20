ad0dc2c763ab8ae5efcde2bb17154630f69f5e47
varun shivaprasad <varun257@gmail.com>
2018-12-20 12:40:19 +0530

    feat(add-vrack-api): vrack api
    add vrack new apis
    add operation api
    
    resolves MBP-210

--- src/cloud/project/cloud-project.v6.service.js
+++ src/cloud/project/cloud-project.v6.service.js
@@ -10,7 +10,7 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectV6", function ($re
     };
 
     var cloudProject = $resource("/cloud/project/:serviceName", {
-        serviceName: "@serviceName"
+        serviceName: "@serviceName",
     }, {
         get: {
             method: "GET",
@@ -39,6 +39,20 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectV6", function ($re
         vrack: {
             url: "/cloud/project/:serviceName/vrack",
             method: "GET"
+        },
+        createVrack: {
+            url: "/cloud/project/:serviceName/vrack",
+            method: "POST",
+            hasBody: false
+        },
+        operations: {
+            method: "GET",
+            isArray: true,
+            url: "/cloud/project/:serviceName/operation"
+        },
+        getOperation: {
+            method: "GET",
+            url: "/cloud/project/:serviceName/operation/:operationId"
         }
     });
 
