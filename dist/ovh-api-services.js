8705f43bc7e98674cbcb1464e1d754138ce82146
CDS Agent <cyrille.bourgois@corp.ovh.com>
2019-04-29 09:39:24 +0000

    Release v6.15.0

--- dist/ovh-api-services.js
+++ dist/ovh-api-services.js
@@ -1337,6 +1337,9 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectInstance", ["$inje
         Aapi: function () {
             return $injector.get("OvhApiCloudProjectInstanceAapi");
         },
+        Interface: function () {
+            return $injector.get("OvhApiCloudProjectInstanceInterface");
+        },
         resetCache: cache.removeAll,
         cache: cache
     };
@@ -1441,6 +1444,60 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectInstanceV6", ["$re
     };
 
     return instancesResource;
+
+}]);
+
+angular.module("ovh-api-services").service("OvhApiCloudProjectInstanceInterface", ["$injector", "$cacheFactory", function ($injector, $cacheFactory) {
+
+    "use strict";
+    var cache = $cacheFactory("OvhApiCloudProjectInstanceInterface");
+
+    return {
+        v6: function () {
+            return $injector.get("OvhApiCloudProjectInstanceInterfaceV6");
+        },
+        resetCache: cache.removeAll,
+        cache: cache
+    };
+
+}]);
+
+angular.module("ovh-api-services").service("OvhApiCloudProjectInstanceInterfaceV6", ["$resource", "OvhApiCloudProjectInstanceInterface", function ($resource, OvhApiCloudProjectInstanceInterface) {
+
+    "use strict";
+
+    var interceptor = {
+        response: function (response) {
+            OvhApiCloudProjectInstanceInterface.resetCache();
+            return response.data;
+        }
+    };
+
+    var interfacesResource = $resource("/cloud/project/:serviceName/instance/:instanceId/interface/:interfaceId", {
+        serviceName: "@serviceName",
+        instanceId: "@instanceId",
+        interfaceId: "@interfaceId"
+    }, {
+        get: { method: "GET", cache: OvhApiCloudProjectInstanceInterface.cache },
+        query: { method: "GET", cache: OvhApiCloudProjectInstanceInterface.cache, isArray: true },
+        save: { method: "POST", interceptor: interceptor },
+        remove: { method: "DELETE", interceptor: interceptor },
+        "delete": { method: "DELETE", interceptor: interceptor }
+    });
+
+    interfacesResource.resetAllCache = function () {
+        OvhApiCloudProjectInstanceInterface.resetCache();
+    };
+
+    interfacesResource.resetCache = function () {
+        OvhApiCloudProjectInstanceInterface.resetCache();
+    };
+
+    interfacesResource.resetQueryCache = function () {
+        OvhApiCloudProjectInstanceInterface.resetCache();
+    };
+
+    return interfacesResource;
 
 }]);
 
