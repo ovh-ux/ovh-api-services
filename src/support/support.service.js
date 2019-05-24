384a16a63d8b0cdc1179391f67aa55376a0ec60a
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-05-24 14:09:05 +0200

    feat: add iceberg support for Support

--- src/support/support.service.js
+++ src/support/support.service.js
@@ -1,6 +1,9 @@
 angular.module("ovh-api-services").service("OvhApiSupport", function ($injector) {
     "use strict";
     return {
+        Iceberg: function () {
+            return $injector.get("OvhApiSupportIceberg");
+        },
         v6: function () {
             return $injector.get("OvhApiSupportV6");
         }
