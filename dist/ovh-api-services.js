4bfaed1b9c93be591a328f0c71a5351fe3a0f2c8
CDS Agent <7319147+FredericEspiau@users.noreply.github.com>
2019-05-27 12:36:01 +0000

    Release v6.21.0

--- dist/ovh-api-services.js
+++ dist/ovh-api-services.js
@@ -17233,9 +17233,22 @@ angular.module("ovh-api-services").service("OvhApiSupplyMondialRelayV6", ["Polle
     return mondialRelay;
 }]);
 
+angular
+    .module("ovh-api-services")
+    .service("OvhApiSupportIceberg", ["iceberg", function (iceberg) {
+        "use strict";
+
+        var alertResource = iceberg("/support/tickets");
+
+        return alertResource;
+    }]);
+
 angular.module("ovh-api-services").service("OvhApiSupport", ["$injector", function ($injector) {
     "use strict";
     return {
+        Iceberg: function () {
+            return $injector.get("OvhApiSupportIceberg");
+        },
         v6: function () {
             return $injector.get("OvhApiSupportV6");
         }
