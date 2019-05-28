b4f457891274e7da06ec353fa365d339a4a6519e
CDS Agent <cyrille.bourgois@corp.ovh.com>
2019-05-28 14:53:41 +0000

    Release v6.22.0

--- dist/ovh-api-services.js
+++ dist/ovh-api-services.js
@@ -914,6 +914,10 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectV6", ["$cacheFacto
             url: "/cloud/project/:serviceName/vrack",
             method: "GET"
         },
+        role: {
+            url: "/cloud/project/:serviceName/role",
+            method: "GET"
+        },
         createVrack: {
             url: "/cloud/project/:serviceName/vrack",
             method: "POST",
@@ -2809,6 +2813,9 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectUser", ["$injector
         },
         Aapi: function () {
             return $injector.get("OvhApiCloudProjectUserAapi");
+        },
+        Role: function () {
+            return $injector.get("OvhApiCloudProjectUserRole");
         }
     };
 
@@ -2859,6 +2866,55 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectUserV6", ["$resour
     };
 
     return users;
+
+}]);
+
+angular.module("ovh-api-services").service("OvhApiCloudProjectUserRole", ["$injector", function ($injector) {
+
+    "use strict";
+
+    return {
+        v6: function () {
+            return $injector.get("OvhApiCloudProjectUserRoleV6");
+        }
+    };
+
+}]);
+
+angular.module("ovh-api-services").service("OvhApiCloudProjectUserRoleV6", ["$resource", "$cacheFactory", function ($resource, $cacheFactory) {
+    "use strict";
+
+    var queryCache = $cacheFactory("OvhApiCloudProjectUserRoleV6Query");
+    var cache = $cacheFactory("OvhApiCloudProjectUserRoleV6");
+
+    var interceptor = {
+        response: function (response) {
+            cache.remove(response.config.url);
+            queryCache.removeAll();
+            return response.data;
+        }
+    };
+
+    var roles = $resource("/cloud/project/:serviceName/user/:userId/role/:roleId", {
+        serviceName: "@serviceName",
+        userId: "@userId",
+        roleId: "@roleId"
+    }, {
+        query: { method: "GET", cache: queryCache, isArray: true },
+        save: { method: "POST", interceptor: interceptor },
+        get: { method: "GET", cache: cache },
+        "delete": { method: "DELETE", interceptor: interceptor }
+    });
+
+    roles.resetCache = function () {
+        cache.removeAll();
+    };
+
+    roles.resetQueryCache = function () {
+        queryCache.removeAll();
+    };
+
+    return roles;
 
 }]);
 
