2c71564055f2f03f259d47c91c382e229c9144cb
CDS Agent <7319147+FredericEspiau@users.noreply.github.com>
2019-06-06 16:12:43 +0000

    Release v6.25.0

--- dist/ovh-api-services.js
+++ dist/ovh-api-services.js
@@ -6628,6 +6628,9 @@ angular.module("ovh-api-services").service("OvhApiDedicatedCloud", ["$injector",
         Option: function () {
             return $injector.get("OvhApiDedicatedCloudOption");
         },
+        ServicePacks: function () {
+            return $injector.get("OvhApiDedicatedCloudServicePacks");
+        },
         Task: function () {
             return $injector.get("OvhApiDedicatedCloudTask");
         },
@@ -7117,6 +7120,56 @@ angular.module("ovh-api-services").service("OvhApiDedicatedCloudOptionV6", ["$re
 
     return optionResource;
 }]);
+
+angular
+    .module("ovh-api-services")
+    .service("OvhApiDedicatedCloudServicePacks", ["$injector", function ($injector) {
+        "use strict";
+
+        return {
+            v6: function () {
+                return $injector.get("OvhApiDedicatedCloudServicePacksV6");
+            }
+        };
+
+    }]);
+
+angular
+    .module("ovh-api-services")
+    .service("OvhApiDedicatedCloudServicePacksV6", ["$resource", "$cacheFactory", function ($resource, $cacheFactory) {
+        "use strict";
+
+        var queryCache = $cacheFactory("OvhApiDedicatedCloudServicePacksV6Query");
+        var cache = $cacheFactory("OvhApiDedicatedCloudServicePacksV6");
+
+        var servicePacksResource = $resource("/dedicatedCloud/:serviceName/servicePacks", {
+            serviceName: "@serviceName"
+        }, {
+            get: {
+                cache: cache,
+                method: "GET",
+                params: {
+                    name: "@name"
+                },
+                url: "/dedicatedCloud/:serviceName/servicePacks/:name"
+            },
+            query: {
+                cache: queryCache,
+                isArray: true,
+                method: "GET"
+            }
+        });
+
+        servicePacksResource.resetCache = function () {
+            cache.removeAll();
+        };
+
+        servicePacksResource.resetQueryCache = function () {
+            queryCache.removeAll();
+        };
+
+        return servicePacksResource;
+    }]);
 
 angular.module("ovh-api-services").service("OvhApiDedicatedCloudTask", ["$injector", function ($injector) {
     "use strict";
