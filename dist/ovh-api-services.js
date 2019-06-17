ac1fdeb3ac98ad986a456a0d39d7c9453077a1aa
CDS Agent <adireddyravindra@gmail.com>
2019-06-17 09:02:38 +0000

    Release v6.28.0

--- dist/ovh-api-services.js
+++ dist/ovh-api-services.js
@@ -549,6 +549,13 @@ angular.module("ovh-api-services").service("OvhApiCloudV6", ["$resource", "OvhAp
         subsidiaryPrice: {
             url: "/cloud/subsidiaryPrice",
             method: "GET"
+        },
+        agreements: {
+            url: "/cloud/agreements",
+            method: "GET",
+            params: {
+                product: "@product"
+            }
         }
     });
 }]);
@@ -2359,6 +2366,7 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectRegionWorkflowBack
 angular.module("ovh-api-services").service("OvhApiCloudProjectRegionWorkflowBackupV6", ["$resource", "$cacheFactory", function ($resource, $cacheFactory) {
     "use strict";
 
+    var cache = $cacheFactory("OvhApiCloudProjectRegionWorkflowBackupV6");
     var queryCache = $cacheFactory("OvhApiCloudProjectRegionWorkflowBackupV6Query");
 
     var interceptor = {
@@ -2368,14 +2376,20 @@ angular.module("ovh-api-services").service("OvhApiCloudProjectRegionWorkflowBack
         }
     };
 
-    var backup = $resource("/cloud/project/:serviceName/region/:regionName/workflow/backup/:backupId", {
+    var backup = $resource("/cloud/project/:serviceName/region/:regionName/workflow/backup/:backupWorkflowId", {
         serviceName: "@serviceName",
-        regionName: "@regionName"
+        regionName: "@regionName",
+        backupWorkflowId: "@backupWorkflowId"
     }, {
         query: { method: "GET", cache: queryCache, isArray: true },
+        get: { method: "GET", cache: cache },
         save: { method: "POST", interceptor: interceptor },
         "delete": { method: "DELETE", interceptor: interceptor }
     });
+
+    backup.resetCache = function () {
+        cache.removeAll();
+    };
 
     backup.resetQueryCache = function () {
         queryCache.removeAll();
