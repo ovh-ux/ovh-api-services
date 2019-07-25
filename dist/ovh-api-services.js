3ff8b7dff82f84f3e42528eed1d1bb4b8c53c825
CDS Agent <frederic.espiau@corp.ovh.com>
2019-07-25 15:40:18 +0000

    Release v7.4.2

--- dist/ovh-api-services.js
+++ dist/ovh-api-services.js
@@ -17709,30 +17709,43 @@ angular.module("ovh-api-services").service("OvhApiSupportV6", ["$resource", "$ca
     var cache = $cacheFactory("OvhApiSupportV6");
     var queryCache = $cacheFactory("OvhApiSupportV6Query");
 
+    var interceptor = {
+        response: function (response) {
+            cache.removeAll();
+            queryCache.removeAll();
+            return response;
+        }
+    };
+
     var support = $resource("/support/tickets/:id", {
         id: "@id"
     }, {
         schema: { method: "GET", url: "/support.json" },
         query: { method: "GET", isArray: true, cache: queryCache },
         get: { method: "GET", cache: cache },
-        getMessages: {
+        queryMessages: {
             method: "GET",
             url: "/support/tickets/:id/messages",
             isArray: true
         },
         create: {
+            interceptor: interceptor,
             method: "POST",
             url: "/support/tickets/:id/create"
         },
         close: {
+            hasBody: false,
+            interceptor: interceptor,
             method: "POST",
             url: "/support/tickets/:id/close"
         },
         reopen: {
+            interceptor: interceptor,
             method: "POST",
             url: "/support/tickets/:id/reopen"
         },
         reply: {
+            interceptor: interceptor,
             method: "POST",
             url: "/support/tickets/:id/reply"
         }
