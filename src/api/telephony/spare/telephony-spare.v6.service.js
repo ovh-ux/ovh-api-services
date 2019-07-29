101d8e3d60ff44a2e1da1adeb24eb6d1d68fad12
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2019-07-29 15:14:04 +0200

    feat(spare): add spare management for telephony and xdsl
    Ref: UXCT-177

---
+++ src/api/telephony/spare/telephony-spare.v6.service.js
@@ -0,0 +1,61 @@
+angular.module("ovh-api-services").service("OvhApiTelephonySpareV6", function ($resource, $cacheFactory) {
+    "use strict";
+
+    var cache = $cacheFactory("OvhApiTelephonySpareV6");
+    var queryCache = $cacheFactory("OvhApiTelephonySpareV6V6Query");
+
+    var interceptor = {
+        response: function (response) {
+            cache.remove(response.config.url);
+            queryCache.removeAll();
+            return response.resource;
+        }
+    };
+
+    var spareResource = $resource("/telephony/spare", {
+        spare: "@spare"
+    }, {
+        query: {
+            method: "GET",
+            isArray: true,
+            cache: queryCache
+        },
+        get: {
+            method: "GET",
+            cache: cache
+        },
+        getSpare: {
+            method: "GET",
+            url: "/telephony/spare/:spare"
+        },
+        replaceSpare: {
+            method: "POST",
+            url: "/telephony/spare/:spare/replace",
+            interceptor: interceptor
+        },
+        deleteSpare: {
+            method: "DELETE",
+            url: "/telephony/spare/:spare",
+            interceptor: interceptor
+        },
+        getBrands: {
+            method: "GET",
+            url: "/telephony/spare/brands",
+        },
+        getNewSpare: {
+            method: "GET",
+            url: "/order/telephony/spare/new",
+        },
+        orderNewSpare: {
+            method: "POST",
+            url: "/order/telephony/spare/new",
+        },
+    });
+
+    spareResource.resetAllCache = function () {
+        cache.removeAll();
+        queryCache.removeAll();
+    };
+
+    return spareResource;
+});
---
+++ src/api/xdsl/spare/xdsl-spare.v6.service.js
@@ -0,0 +1,66 @@
+angular.module("ovh-api-services").service("OvhApiXdslSpareV6", function ($resource, $cacheFactory) {
+    "use strict";
+
+    var cache = $cacheFactory("OvhApiXdslSpareV6");
+    var queryCache = $cacheFactory("OvhApiXdslSpareV6Query");
+
+    var interceptor = {
+        response: function (response) {
+            cache.remove(response.config.url);
+            queryCache.removeAll();
+            return response.resource;
+        }
+    };
+
+    var spareResource = $resource("/xdsl/spare", {
+        spare: "@spare"
+    }, {
+        query: {
+            method: "GET",
+            isArray: true,
+            cache: queryCache
+        },
+        get: {
+            method: "GET",
+            cache: cache
+        },
+        getSpare: {
+            method: "GET",
+            url: "/xdsl/spare/:spare"
+        },
+        replaceSpare: {
+            method: "POST",
+            url: "/xdsl/spare/:spare/replace",
+            interceptor: interceptor
+        },
+        returnMerchandise: {
+            method: "POST",
+            url: "xdsl/spare/:spare/returnMerchandise",
+            interceptor: interceptor
+        },
+        deleteSpare: {
+            method: "DELETE",
+            url: "/xdsl/spare/:spare",
+            interceptor: interceptor
+        },
+        getBrands: {
+            method: "GET",
+            url: "/xdsl/spare/brands",
+        },
+        getNewSpare: {
+            method: "GET",
+            url: "/order/xdsl/spare/new",
+        },
+        orderNewSpare: {
+            method: "POST",
+            url: "/order/xdsl/spare/new",
+        },
+    });
+
+    spareResource.resetAllCache = function () {
+        cache.removeAll();
+        queryCache.removeAll();
+    };
+
+    return spareResource;
+});
