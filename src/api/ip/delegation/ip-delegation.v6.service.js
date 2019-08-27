154caeaa6987a206350237064466a715a6cf830a
Marie JONES <marie.jones@corp.ovh.com>
2019-08-27 09:09:06 +0200

    fix(ip.delegation): don't pass target as url param
    DTRUN-2871

--- src/api/ip/delegation/ip-delegation.v6.service.js
+++ src/api/ip/delegation/ip-delegation.v6.service.js
@@ -1,4 +1,9 @@
 angular.module('ovh-api-services').service('OvhApiIpDelegationV6', $resource => $resource('/ip/:ip/delegation/:target', {
   ip: '@ip',
   target: '@target',
+}, {
+  save: {
+    method: 'POST',
+    url: '/ip/:ip/delegation',
+  },
 }));
