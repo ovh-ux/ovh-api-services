f7cc524ed5e9ed6f1a2e6bfc4c82570a8acae257
Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>
2019-09-26 10:21:50 +0200

    fix(vps): add cache for available images queries

--- src/api/vps/images/available/vps-images-available.v6.service.js
+++ src/api/vps/images/available/vps-images-available.v6.service.js
@@ -1,6 +1,26 @@
-angular.module('ovh-api-services').service('OvhApiVpsImagesAvailableV6', $resource => $resource('/vps/:serviceName/images/available', {
-  serviceName: '@serviceName',
-}, {
-  query: { method: 'GET', isArray: true },
-  get: { method: 'GET', url: '/vps/:serviceName/images/available/:id' },
-}));
+angular.module('ovh-api-services').service('OvhApiVpsImagesAvailableV6', ($cacheFactory, $resource) => {
+  const cache = $cacheFactory('OvhApiVpsImagesAvailableV6');
+  const queryCache = $cacheFactory('OvhApiVpsImagesAvailableV6');
+
+  const resource = $resource('/vps/:serviceName/images/available', {
+    serviceName: '@serviceName',
+  }, {
+    query: { method: 'GET', isArray: true, cache: queryCache },
+    get: { method: 'GET', url: '/vps/:serviceName/images/available/:id', cache },
+  });
+
+  resource.resetCache = () => {
+    cache.removeAll();
+  };
+
+  resource.resetQueryCache = () => {
+    queryCache.removeAll();
+  };
+
+  resource.resetAllCache = () => {
+    resource.resetCache();
+    resource.resetQueryCache();
+  };
+
+  return resource;
+});
