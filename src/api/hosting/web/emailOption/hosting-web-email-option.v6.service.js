a60554f232b9bd651a61af86959c6d6fb43df79a
Ravindra Adireddy <adireddyravindra@gmail.com>
2019-12-10 11:55:25 +0530

    feat(web): added hosting email option end points

---
+++ src/api/hosting/web/emailOption/hosting-web-email-option.v6.service.js
@@ -0,0 +1,34 @@
+angular.module('ovh-api-services').service('OvhApiHostingWebEmailOptionV6', ($resource, $cacheFactory) => {
+  const cache = $cacheFactory('OvhApiHostingWebEmailOptionv6Cache');
+  const queryCache = $cacheFactory('OvhApiHostingWebEmailOptionv6CacheQuery');
+
+  const resource = $resource('/hosting/web/:serviceName/emailOption/:id', {
+    serviceName: '@serviceName',
+    id: '@id',
+  }, {
+    query: {
+      method: 'GET',
+      cache: queryCache,
+      isArray: true,
+    },
+    get: {
+      method: 'GET',
+      cache,
+    },
+    serviceInfo: {
+      method: 'GET',
+      url: '/hosting/web/:serviceName/emailOption/:id/serviceInfos',
+      cache,
+    }
+  });
+
+  resource.resetCache = function () {
+    cache.removeAll();
+  };
+
+  resource.resetQueryCache = function () {
+    queryCache.removeAll();
+  };
+
+  return resource;
+});
--- src/api/hosting/web/web.service.js
+++ src/api/hosting/web/web.service.js
@@ -8,6 +8,9 @@ angular.module('ovh-api-services').service('OvhApiHostingWeb', ($injector) => ({
   Ssl() {
     return $injector.get('OvhApiHostingWebSsl');
   },
+  EmailOption() {
+    return $injector.get('OvhApiHostingWebEmailOption');
+  },
   v6() {
     return $injector.get('OvhApiHostingWebV6');
   },
