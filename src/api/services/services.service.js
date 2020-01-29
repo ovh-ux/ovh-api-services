85d54f08885bb49d80a5811f83efb01c71608ef6
JeremyDec <34273689+JeremyDec@users.noreply.github.com>
2020-01-29 09:59:24 +0100

    feat(services): add detach routes (#276)
    Signed-off-by: Jérémy De-Cesare <jeremy.de-cesare@corp.ovh.com>

--- src/api/services/services.service.js
+++ src/api/services/services.service.js
@@ -2,6 +2,9 @@ angular.module('ovh-api-services').service('OvhApiServices', ($injector) => ({
   Aapi() {
     return $injector.get('OvhApiServicesAapi');
   },
+  Detach() {
+    return $injector.get('OvhApiServicesDetach');
+  },
   v6() {
     return $injector.get('OvhApiServicesV6');
   },
