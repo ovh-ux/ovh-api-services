75e64db7355c6f223a7f19863d7d84e94b25df2e
Stephanie Moallic <stephanie.moallic@corp.ovh.com>
2020-02-06 09:50:28 +0100

    feat(xdsl.modem): add endpoint to get price for modem exchange

--- src/api/xdsl/modem/xdsl-modem.v6.service.js
+++ src/api/xdsl/modem/xdsl-modem.v6.service.js
@@ -27,5 +27,9 @@ angular.module('ovh-api-services').service('OvhApiXdslModemV6', ($resource, OvhA
       url: '/xdsl/:xdslId/modem/comfortExchange',
       interceptor,
     },
+    comfortExchangeLastModel: {
+      method: 'GET',
+      url: '/xdsl/:xdslId/modem/comfortExchange',
+    },
   });
 });
