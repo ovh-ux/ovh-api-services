db7e19d8a36ce898a323f13244f84d917632cb69
Antoine Leblanc <ant.leblanc@gmail.com>
2020-06-16 10:27:56 +0200

    feat(connectivity.eligibility): add endpoints
    - Eligibility test for copper and fiber lines
    - Connectivity searches for address and buildings
    
    ref: UXCT-193
    
    Signed-off-by: Antoine Leblanc <ant.leblanc@gmail.com>
    Co-authored-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- src/api/connectivity/eligibility/connectivity-eligibility.v6.service.js
+++ src/api/connectivity/eligibility/connectivity-eligibility.v6.service.js
@@ -1,8 +1,101 @@
-angular.module('ovh-api-services').service('OvhApiConnectivityEligibilityV6', ($resource, OvhApiConnectivityEligibility) => $resource('/connectivity/eligibility/search/buildingDetails ', {
-}, {
-  buildingDetails: {
-    method: 'POST',
-    isArray: false,
-    cache: OvhApiConnectivityEligibility.cache,
-  },
-}));
+angular.module('ovh-api-services').service('OvhApiConnectivityEligibilityV6', ($resource, Poller) => {
+  const eligibility = $resource('/connectivity/eligibility', {
+  }, {
+    eligibility: {
+      url: '/connectivity/eligibility/test ',
+      method: 'GET',
+      isArray: false,
+      params: {
+        eligibilityReference: '@eligibilityReference',
+      },
+    },
+  });
+
+  eligibility.testAddress = function ($scope, opts) {
+    const url = '/connectivity/eligibility/test/address';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          streetCode: opts.streetCode,
+          streetNumber: opts.streetNumber,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'POST',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  eligibility.testBuilding = function ($scope, opts) {
+    const url = '/connectivity/eligibility/test/building';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          building: opts.building,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'POST',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  eligibility.testLine = function ($scope, opts) {
+    const url = '/connectivity/eligibility/test/line';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          lineNumber: opts.lineNumber,
+          status: opts.status,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'POST',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  return eligibility;
+});
--- src/api/connectivity/eligibility/search/connectivity-eligibility-search.service.js
+++ src/api/connectivity/eligibility/search/connectivity-eligibility-search.service.js
@@ -1,5 +1,11 @@
-angular.module('ovh-api-services').service('OvhApiConnectivityEligibilitySearch', ($injector) => ({
-  v6() {
-    return $injector.get('OvhApiConnectivityEligibilitySearchV6');
-  },
-}));
+angular.module('ovh-api-services').service('OvhApiConnectivityEligibilitySearch', ($injector, $cacheFactory) => {
+  const cache = $cacheFactory('OvhApiConnectivityEligibilitySearch');
+
+  return {
+    v6() {
+      return $injector.get('OvhApiConnectivityEligibilitySearchV6');
+    },
+    resetCache: cache.removeAll,
+    cache,
+  };
+});
