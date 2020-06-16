873633aa0285eeab3ed84ae0b7f5610d17ff2968
Antoine Leblanc <ant.leblanc@gmail.com>
2020-06-16 10:27:56 +0200

    feat(connectivity.eligibility): add endpoints
    - Eligibility test for copper and fiber lines
    - Connectivity searches for address and buildings
    
    ref: UXCT-193
    
    Signed-off-by: Antoine Leblanc <ant.leblanc@gmail.com>
    Co-authored-by: Stephanie Moallic <stephanie.moallic@corp.ovh.com>

--- src/api/connectivity/eligibility/search/connectivity-eligibility-search.v6.service.js
+++ src/api/connectivity/eligibility/search/connectivity-eligibility-search.v6.service.js
@@ -1,19 +1,186 @@
-angular.module('ovh-api-services').service('OvhApiConnectivityEligibilitySearchV6', ($resource) => $resource('/connectivity/eligibility/search', {
-}, {
-  searchCities: {
-    url: '/connectivity/eligibility/search/cities',
-    method: 'POST',
-    isArray: false,
-    params: {
-      zipCode: '@zipCode',
+angular.module('ovh-api-services').service('OvhApiConnectivityEligibilitySearchV6', ($resource, OvhApiConnectivityEligibilitySearch, Poller) => {
+  const eligibilitySearch = $resource('/connectivity/eligibility/search', {
+  }, {
+    buildingDetails: {
+      url: '/connectivity/eligibility/search/buildingDetails',
+      method: 'POST',
+      isArray: false,
+      cache: OvhApiConnectivityEligibilitySearch.cache,
     },
-  },
-  searchStreets: {
-    url: '/connectivity/eligibility/search/streets',
-    method: 'POST',
-    isArray: false,
-    params: {
-      inseeCode: '@inseeCode',
-    },
-  },
-}));
+  });
+
+  eligibilitySearch.searchCities = function ($scope, opts) {
+    const url = '/connectivity/eligibility/search/cities';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          zipCode: opts.zipCode,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'POST',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  eligibilitySearch.searchStreets = function ($scope, opts) {
+    const url = '/connectivity/eligibility/search/streets';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          inseeCode: opts.inseeCode,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'POST',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  eligibilitySearch.searchBuildingByLines = function ($scope, opts) {
+    const url = '/connectivity/eligibility/search/buildingsByLine';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          lineNumber: opts.lineNumber,
+          status: opts.status,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'POST',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  eligibilitySearch.searchBuildings = function ($scope, opts) {
+    const url = '/connectivity/eligibility/search/buildings';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          streetCode: opts.streetCode,
+          streetNumber: opts.streetNumber,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'POST',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  eligibilitySearch.searchLines = function ($scope, opts) {
+    const url = '/connectivity/eligibility/search/lines';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          streetCode: opts.streetCode,
+          streetNumber: opts.streetNumber,
+          ownerName: opts.ownerName,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'POST',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  eligibilitySearch.searchMeetings = function ($scope, opts) {
+    const url = '/connectivity/eligibility/search/meetings';
+
+    $scope.$on('$destroy', () => {
+      Poller.kill({
+        scope: $scope.$id,
+      });
+    });
+
+    return Poller.poll(
+      url,
+      null,
+      {
+        postData: {
+          eligibilityReference: opts.eligibilityReference,
+          productCode: opts.productCode,
+        },
+        successRule: {
+          status(elem) {
+            return elem.status === 'error' || elem.status === 'ok';
+          },
+        },
+        scope: $scope.$id,
+        method: 'POST',
+        retryMaxAttempts: 3,
+      },
+    );
+  };
+
+  return eligibilitySearch;
+});
