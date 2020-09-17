68a8be0bf70e7fd3c8879de8d3fcaed8ad26e026
Corentin Regal <corentin.regal@corp.ovh.com>
2020-09-17 09:03:16 +0200

    feat(cloud.project.serving): fix indent

--- src/api/cloud/project/ai/capabilities/training/region/gpu/cloud-project-ai-capabilities-training-region-gpu.v6.service.js
+++ src/api/cloud/project/ai/capabilities/training/region/gpu/cloud-project-ai-capabilities-training-region-gpu.v6.service.js
@@ -1,6 +1,6 @@
 angular
   .module('ovh-api-services')
   .service('OvhApiCloudProjectAiCapabilitiesTrainingRegionGpuV6', ($resource) => $resource('/cloud/project/:serviceName/ai/capabilities/training/region/:region/gpu', {
-      serviceName: '@serviceName',
-      region: '@region',
-    }));
+    serviceName: '@serviceName',
+    region: '@region',
+  }));
