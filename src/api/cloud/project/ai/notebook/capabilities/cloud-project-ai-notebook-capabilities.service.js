a8e53858a6857f5069f6f76f165422399e42bf23
Adrien Carreira <adrien.carreira@ovhcloud.com>
2021-05-21 16:20:50 +0200

    Fix typo

--- src/api/cloud/project/ai/notebook/capabilities/cloud-project-ai-notebook-capabilities.service.js
+++ src/api/cloud/project/ai/notebook/capabilities/cloud-project-ai-notebook-capabilities.service.js
@@ -1,6 +1,6 @@
 angular
   .module('ovh-api-services')
   .service('OvhApiCloudProjectAiNotebookCapabilities', ($injector) => ({
-    Editor: () => $injector.get('=Editor'),
+    Editor: () => $injector.get('OvhApiCloudProjectAiNotebookCapabilitiesEditor'),
     Framework: () => $injector.get('OvhApiCloudProjectAiNotebookCapabilitiesFramework'),
   }));
