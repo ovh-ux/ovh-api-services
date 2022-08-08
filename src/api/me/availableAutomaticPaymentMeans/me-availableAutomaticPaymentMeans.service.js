d2f7cb4075c35f00f5704e6966323a9ae1c9a9a6
Anoop N <anooparveti@gmail.com>
2022-08-08 13:06:43 +0530

    refactor: remove deprecated `/me/avaiableAutomaticPaymentMeans` method (#340)
    Signed-off-by: Anoop <anooparveti@gmail.com>
    
    BREAKING-CHANGE: drop me.AvailableAutomaticPaymentMeans method

--- src/api/me/availableAutomaticPaymentMeans/me-availableAutomaticPaymentMeans.service.js
+++
@@ -1,5 +0,0 @@
-angular.module('ovh-api-services').service('OvhApiMeAvailableAutomaticPaymentMeans', ($injector) => ({
-  v6() {
-    return $injector.get('OvhApiMeAvailableAutomaticPaymentMeansV6');
-  },
-}));
